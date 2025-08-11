96be31ae88c4bb9eb76f992acab5bacc3a74bea2
krypton <myersalp@gmail.com>
2025-08-12 00:00:33 +0300

    http server

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use actix_web::{get, App, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    "kn"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("The server has started: http://localhost:8080");
+    
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
